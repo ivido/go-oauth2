2150d60433a908a3b289994b41484d09a1eb07ea
Jari Maijenburg <jari.maijenburg@ivido.nl>
2022-04-07 10:13:27 +0200

    Moved around some parameters

--- errors/oauth.go
+++ errors/oauth.go
@@ -1,16 +1,16 @@
 package errors
 
 type OAuthError struct {
+	StatusCode  int
 	Code        string
 	Description string
-	StatusCode  int
 }
 
-func NewOAuthError(code string, description string, statusCode int) *OAuthError {
+func NewOAuthError(statusCode int, code string, description string) *OAuthError {
 	return &OAuthError{
+		StatusCode:  statusCode,
 		Code:        code,
 		Description: description,
-		StatusCode:  statusCode,
 	}
 }
 
@@ -30,17 +30,17 @@ var (
 
 // OAuth errors
 var (
-	ErrInvalidRequest                 = NewOAuthError(ErrCodeInvalidRequest, "The request is missing a required parameter includes an invalid parameter value includes a parameter more than once or is otherwise malformed", 400)
-	ErrUnauthorizedClient             = NewOAuthError(ErrCodeUnauthorizedClient, "The client is not authorized to request an authorization code using this method", 401)
-	ErrAccessDenied                   = NewOAuthError(ErrCodeAccessDenied, "The resource owner or authorization server denied the request", 403)
-	ErrUnsupportedResponseType        = NewOAuthError(ErrCodeUnsupportedResponseType, "The authorization server does not support obtaining an authorization code using this method", 401)
-	ErrInvalidScope                   = NewOAuthError(ErrCodeInvalidScope, "The requested scope is invalid unknown or malformed", 400)
-	ErrServerError                    = NewOAuthError(ErrCodeServerError, "The authorization server encountered an unexpected condition that prevented it from fulfilling the request", 500)
-	ErrTemporarilyUnavailable         = NewOAuthError(ErrCodeTemporarilyUnavailable, "The authorization server is currently unable to handle the request due to a temporary overloading or maintenance of the server", 503)
-	ErrInvalidClient                  = NewOAuthError(ErrCodeInvalidClient, "Client authentication failed", 401)
-	ErrInvalidGrant                   = NewOAuthError(ErrCodeInvalidGrant, "The provided authorization grant (e.g. authorization code resource owner credentials) or refresh token is invalid expired revoked does not match the redirection URI used in the authorization request or was issued to another client", 401)
-	ErrUnsupportedGrantType           = NewOAuthError(ErrCodeUnsupportedGrantType, "The authorization grant type is not supported by the authorization server", 401)
-	ErrCodeChallengeRquired           = NewOAuthError(ErrCodeInvalidRequest, "PKCE is required. code_challenge is missing", 400)
-	ErrUnsupportedCodeChallengeMethod = NewOAuthError(ErrCodeInvalidRequest, "Selected code_challenge_method not supported", 400)
-	ErrInvalidCodeChallengeLen        = NewOAuthError(ErrCodeInvalidRequest, "Code challenge length must be between 43 and 128 charachters long", 400)
+	ErrInvalidRequest                 = NewOAuthError(400, ErrCodeInvalidRequest, "The request is missing a required parameter includes an invalid parameter value includes a parameter more than once or is otherwise malformed")
+	ErrUnauthorizedClient             = NewOAuthError(401, ErrCodeUnauthorizedClient, "The client is not authorized to request an authorization code using this method")
+	ErrAccessDenied                   = NewOAuthError(403, ErrCodeAccessDenied, "The resource owner or authorization server denied the request")
+	ErrUnsupportedResponseType        = NewOAuthError(401, ErrCodeUnsupportedResponseType, "The authorization server does not support obtaining an authorization code using this method")
+	ErrInvalidScope                   = NewOAuthError(400, ErrCodeInvalidScope, "The requested scope is invalid unknown or malformed")
+	ErrServerError                    = NewOAuthError(500, ErrCodeServerError, "The authorization server encountered an unexpected condition that prevented it from fulfilling the request")
+	ErrTemporarilyUnavailable         = NewOAuthError(503, ErrCodeTemporarilyUnavailable, "The authorization server is currently unable to handle the request due to a temporary overloading or maintenance of the server")
+	ErrInvalidClient                  = NewOAuthError(401, ErrCodeInvalidClient, "Client authentication failed")
+	ErrInvalidGrant                   = NewOAuthError(401, ErrCodeInvalidGrant, "The provided authorization grant (e.g. authorization code resource owner credentials) or refresh token is invalid expired revoked does not match the redirection URI used in the authorization request or was issued to another client")
+	ErrUnsupportedGrantType           = NewOAuthError(401, ErrCodeUnsupportedGrantType, "The authorization grant type is not supported by the authorization server")
+	ErrCodeChallengeRquired           = NewOAuthError(400, ErrCodeInvalidRequest, "PKCE is required. code_challenge is missing")
+	ErrUnsupportedCodeChallengeMethod = NewOAuthError(400, ErrCodeInvalidRequest, "Selected code_challenge_method not supported")
+	ErrInvalidCodeChallengeLen        = NewOAuthError(400, ErrCodeInvalidRequest, "Code challenge length must be between 43 and 128 charachters long")
 )
