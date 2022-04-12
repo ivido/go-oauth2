2e944018d8737ada389b49f4d9b809ded6801a28
Jari Maijenburg <jari.maijenburg@ivido.nl>
2022-04-12 17:44:13 +0200

    Added some basic tests

---
+++ server/server_test.go
@@ -0,0 +1,30 @@
+package server_test
+
+import (
+	"testing"
+
+	"github.com/ivido/go-oauth2/auth"
+	"github.com/ivido/go-oauth2/oauth2"
+	"github.com/ivido/go-oauth2/server"
+)
+
+func TestNewDefaultServer(t *testing.T) {
+	cfg := server.NewDefaultConfig()
+	srv := server.NewDefaultServer()
+
+	if srv.Config != cfg {
+		t.Errorf("Server config should be %+v, got %+v", cfg, srv.Config)
+	}
+}
+
+func TestNewServer(t *testing.T) {
+	cfg := server.Config{
+		TokenType: oauth2.TokenTypeOpaque,
+	}
+
+	srv := server.NewServer(cfg)
+
+	if srv.Config != cfg {
+		t.Errorf("Server config should be %+v, got %+v", cfg, srv.Config)
+	}
+}
